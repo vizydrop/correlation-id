6495055ff8ff2cd66bc8d65108f3293a719415ce
Eugene Kisel <kisel@targetprocess.com>
2019-01-18 14:03:52 +0300

    correlation id lib

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-const cls = require(`cls-hooked`);
-const uuid = require(`uuid`);
+const createCorrelationId = require(`./lib/create-correlation-id`); // eslint-disable-line
 
-module.exports = (opts = {}) => {
-    if (opts.generateDefaultId && typeof opts.generateDefaultId !== `function`) {
-        throw new Error(`generateDefaultId must be a function`);
-    }
-
-    if (opts.namespaceName && typeof opts.namespaceName !== `string`) {
-        throw new Error(`namespaceName must be a string`);
-    }
-
-    const store = cls.createNamespace(opts.namespaceName || `correlation-id-namespace`);
-
-    const generateDefaultId = () => {
-        return opts.generateDefaultId ? opts.generateDefaultId() : uuid.v4();
-    };
-
-    const CORRELATION_ID_KEY = `correlation-id`;
-
-    const withId = (id, fn) => store.run(() => {
-        store.set(CORRELATION_ID_KEY, id || generateDefaultId());
-        fn();
-    });
-
-    const getId = () => store.get(CORRELATION_ID_KEY);
-    const bindEmitter = (emitter) => store.bindEmitter(emitter);
-    const bind = (fn) => store.bind(fn);
-
-    return {
-        withId,
-        getId,
-        bindEmitter,
-        bind
-    }
-};
+module.exports = createCorrelationId;
