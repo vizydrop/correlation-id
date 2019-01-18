4d45e1982c2df9d48205b2231893a9fa3949680b
Eugene Kisel <kisel@targetprocess.com>
2019-01-18 09:17:12 +0300

    correlator

---
+++ index.js
@@ -0,0 +1,34 @@
+const cls = require(`cls-hooked`);
+const uuid = require(`uuid`);
+
+module.exports = (opts = {}) => {
+    if (opts.generateDefaultId && typeof opts.generateDefaultId !== `function`) {
+        throw new Error(`generateDefaultId must be a function`);
+    }
+
+    if (opts.namespaceName && typeof opts.namespaceName !== `string`) {
+        throw new Error(`namespaceName must be a string`);
+    }
+
+    const store = cls.createNamespace(opts.namespaceName || `correlation-id-namespace`);
+
+    const generateDefaultId = () => {
+        return opts.generateDefaultId ? opts.generateDefaultId() : uuid.v4();
+    };
+
+    const CORRELATION_ID_KEY = `correlation-id`;
+
+    const withId = (id, fn) => store.run(() => {
+        store.set(CORRELATION_ID_KEY, id || generateDefaultId());
+        fn();
+    });
+
+    const getId = () => store.get(CORRELATION_ID_KEY);
+
+    const bindEmitter = (emitter) => store.bindEmitter(emitter);
+    return {
+        withId,
+        getId,
+        bindEmitter,
+    }
+};
